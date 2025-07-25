1bdc4596e55460f25ed561f3a458be6c0f17d8d8
Thiago <thiagovakdez@gmail.com>
2025-07-24 21:56:11 -0300

    Agrego pantalla de inicio y menu principal con botón "jugar"

---
+++ core/src/main/java/pantallas/JuegoPrincipal.java
@@ -0,0 +1,46 @@
+package pantallas;
+
+import com.badlogic.gdx.Screen;
+import io.github.principal.Principal;
+
+public class JuegoPrincipal implements Screen {
+
+    public JuegoPrincipal (Principal game) {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float v) {
+
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
